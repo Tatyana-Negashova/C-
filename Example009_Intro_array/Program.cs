8609d49d79315d11c446d84fc934e1d137c252ed
tanchik <negashovata@gmail.com>
2022-07-05 20:49:51 +0300

    добавлена задача 11

---
+++ Example009_Intro_array/Program.cs
@@ -0,0 +1,18 @@
+﻿//поиск максимального числа bp трех
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 14, 42, 43, 44, 65, 466, 97, 58, 19 };
+array[0] = 12;// каким образом обратиться к массиву и записать в него значение
+System.Console.WriteLine(array[4]);//обратиться к массиву и получить значение элемента по индексу
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+System.Console.WriteLine(max);
---
+++ Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection) // наполняем массив. int collection -название массива 
+{
+    int length = collection.Length;  //получаем длину массива
+    int index = 0;
+    while (index < length) // пока индекс элемента меньше длины массива
+    {
+        collection[index] = new Random().Next(1, 10); // кладем новое случайное число на позицию индекса из диапазона 1-10
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //метод,который будет печатать массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) //аргумент-массив collection + элемент find
+{
+
+    int count = collection.Length; //определяем количество элементов
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];  // создай массив из 10 элементов. По умолчанию наполнен нулями
+FillArray(array); //метод для наполнения массива
+PrintArray(array); // метод для вывода массив на экран
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 6);
+System.Console.WriteLine(pos);
